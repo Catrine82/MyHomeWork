1e6ea2a5020cda7d77fe3c489aae0949a8eeb716
Catrine82 <fateeva-ea@serviceit.su>
2022-10-29 23:54:47 +0300

    HW9

---
+++ HW9/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+void Nat(int m, int n)
+{
+    Console.Write($"{Math.Min(m,n)} ");
+    if (Math.Min(m,n) < Math.Max(m,n))
+    {
+        Nat(Math.Min(m,n)+1,Math.Max(m,n));
+    }
+}
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Nat(m,n);
+*/
+/*
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int Nat(int m, int n)
+{
+    if (m == n)
+        return m;
+    else
+    if (Math.Min(m,n) < Math.Max(m,n))
+        return Nat(Math.Min(m,n)+1,Math.Max(m,n)) + Math.Min(m,n);
+        else return 0;
+}
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"M = {m}, N = {n} -> {Nat(m,n)}");
+*/
+/*
+// НЕ СМОГЛА  МОЖНО ЕЩЕ ВЕРЕМЕНИ?? Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+double Nat(double m, double n)
+{
+    if (m == 0) return n+1; else
+    if (m==1) return n+2; else
+    if (m=2) return 2*n+3;
+    else return 
+    
+}
+Console.Write("Input M: ");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input N: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.Write($"M = {m}, N = {n} -> {Nat(m,n)}");
+*/
